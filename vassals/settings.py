8e4afe1270c9f7cf9421e2c029f67d4c3c8fba0b
szk333 <Margonem11>
2019-10-18 14:53:08 +0200

    static deployment

--- vassals/settings.py
+++ vassals/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 AUTH_USER_MODEL = 'users.User'
 
 LOGOUT_REDIRECT_URL = 'home'
