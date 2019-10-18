2e6b8f52f2ab0e8bebde940acd225a0f3a778af3
szk333 <Margonem11>
2019-10-18 14:46:40 +0200

    create diplomats and tooltips working

--- vassals/settings.py
+++ vassals/settings.py
@@ -129,10 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 AUTH_USER_MODEL = 'users.User'
 
 LOGOUT_REDIRECT_URL = 'home'
